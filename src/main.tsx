8de1d90290f908f646ede309d1c709559e991a35
sarghant <sghant456@yahoo.com>
2023-06-24 02:47:47 +0400

    Remove unnecessary files and dependencies.

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import { UserCommentsProvider } from "./context/UserCommentsContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserCommentsProvider>
-        <App />
-      </UserCommentsProvider>
-    </BrowserRouter>
+    <UserCommentsProvider>
+      <App />
+    </UserCommentsProvider>
   </React.StrictMode>
 );
